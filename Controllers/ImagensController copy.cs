7035f2062e3940d289475328b9b8e72d7a19e5fc
Guilherme Santos Araújo <guilherme1984.araujo@gmail.com>
2023-04-30 19:52:17 -0300

    Site Fan Merida

---
+++ Controllers/ImagensController copy.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using aula20._04.Models;
+
+namespace aula20._04.Controllers;
+
+public class ImagensController : Controller
+{
+    private readonly ILogger<ImagensController> _logger;
+
+    public ImagensController(ILogger<ImagensController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aula20._04.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
